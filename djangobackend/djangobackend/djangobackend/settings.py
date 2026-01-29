6594bc1ae4af425d2de0bfc5c94ac19b12abc3d0
unknown <rachitkumarsingh144@gmail.com>
2026-01-29 17:50:01 +0530

    ready for deployment

--- djangobackend/djangobackend/djangobackend/settings.py
+++ djangobackend/djangobackend/djangobackend/settings.py
@@ -4,10 +4,11 @@ from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR.parent / '.env.dev')
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
