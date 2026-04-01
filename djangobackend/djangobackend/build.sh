1be445d797b7f80941ba59017f0941e8d49b8feb
unknown <rachitkumarsingh144@gmail.com>
2026-04-01 11:26:44 +0530

    Auto-generate persistent admin for showcase database

--- djangobackend/djangobackend/build.sh
+++ djangobackend/djangobackend/build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create a persistent superuser for the showcase, even if SQLite drops
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@admin.com').exists() or User.objects.create_superuser('admin', 'admin@admin.com', 'admin')"
