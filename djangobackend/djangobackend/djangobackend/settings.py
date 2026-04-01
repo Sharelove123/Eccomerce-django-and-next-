e245bbe5ac5d5b9b82a0994c59500eabf346af97
unknown <rachitkumarsingh144@gmail.com>
2026-04-01 13:38:05 +0530

    Disable persistent connections (conn_max_age=0) for Supabase pooler compatibility

--- djangobackend/djangobackend/djangobackend/settings.py
+++ djangobackend/djangobackend/djangobackend/settings.py
@@ -148,7 +148,7 @@ import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
+        conn_max_age=0,
         conn_health_checks=True,
     )
 } 
