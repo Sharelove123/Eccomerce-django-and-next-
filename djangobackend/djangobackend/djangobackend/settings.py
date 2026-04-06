4125fe0859d99329a210af71a3f93704bc041963
unknown <rachitkumarsingh144@gmail.com>
2026-04-06 12:18:31 +0530

    fix: revert to StaticFilesStorage to avoid MissingFileError in whitenoise

--- djangobackend/djangobackend/djangobackend/settings.py
+++ djangobackend/djangobackend/djangobackend/settings.py
@@ -272,7 +272,7 @@ STORAGES = {
         'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
     }
 }
 
