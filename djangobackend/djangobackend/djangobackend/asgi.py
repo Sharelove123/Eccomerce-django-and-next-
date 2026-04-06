0425712c62a7a7221eaf4e49cf8288841f11717e
unknown <rachitkumarsingh144@gmail.com>
2026-04-06 11:44:41 +0530

    fix(backend): reorder asgi.py imports to ensure Django is initialized before app modules

--- djangobackend/djangobackend/djangobackend/asgi.py
+++ djangobackend/djangobackend/djangobackend/asgi.py
@@ -8,17 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobackend.settings')
+django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-
 from chat.routing import websocket_urlpatterns
 from chat.ws_middleware import QueryStringTokenAuthMiddlewareStack
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobackend.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
